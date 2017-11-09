26bbe0670ebeac2a9fc5b6c4345b5d3329f8e03c
songwei <songwei@e-eduspace.com>
2017-11-09 15:01:58 +0800

    更新根据商品和价格查询符合条件的用户优惠码列表，更新服务的拦截工具实现逻辑

--- edusky-promotion-service/src/main/java/com/edu/sky/promotion/aop/ParamsAspect.java
+++ edusky-promotion-service/src/main/java/com/edu/sky/promotion/aop/ParamsAspect.java
@@ -2,10 +2,10 @@ package com.edu.sky.promotion.aop;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
+import com.edu.sky.core.exception.ResultBean;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,64 +31,56 @@ public class ParamsAspect {
     @Pointcut(value = "execution(* com.edu.sky.promotion.service.*.*(..))")
     public void servicePoint(){}
 
-    @Before(value = "servicePoint()")
-    public void serviceBefore(JoinPoint point) {
-        paramHandle(point,null);
-    }
-    @AfterReturning(value = "servicePoint()",returning = "result")
-    public void serviceAfterReturn(JoinPoint point,Object result) {
-        paramHandle(point,result);
-    }
-
-    private void paramHandle(JoinPoint point,Object result){
+    @Around(value = "servicePoint()")
+    public Object serviceAround(ProceedingJoinPoint pjp){
         try {
-            String methodName = point.getSignature().getName();
-
-            String className = point.getTarget().getClass().getName();
+            String methodName = pjp.getSignature().getName();
+            String className = pjp.getTarget().getClass().getName();
             Class<?> clazz = Class.forName(className);
             Method[] methods = clazz.getMethods();
             List<Method> methodList = new ArrayList<Method>(Arrays.asList(methods));
             Method method = methodList.stream().filter(m -> m.getName().equals(methodName)).findFirst().get();
             int count = method.getParameterCount();
             if (count == 0) {
-                logger.info("\n     ------> 请求接口：" + point.getTarget().getClass().getName() + "." + methodName + "()" +
-                            "\n     ------> 参数为：改接口无参数");
-                return;
-            }
-            List<String> paramNames =getParamNames(method);//参数名称
-            String name = point.getTarget().getClass().getSimpleName() + "." + methodName + "{0}";
-            Class<?>[] parameterTypes = method.getParameterTypes();
-            Object[] args = point.getArgs();
-            if (paramNames.size() != parameterTypes.length) {
-                logger.info("\n     ------> 请求接口：" + name +
-                        "\n     ------> 参数名称为：" + JSON.toJSONString(paramNames) +
-                        "\n     ------> 参数为：" + JSON.toJSONString(args)
-                );
-                return;
-            }
-            StringBuilder sb = new StringBuilder();
-            sb.append("(");
-            for (int i = 0; i < parameterTypes.length;i++) {
-                if ((i+1) == parameterTypes.length) {
-                    sb.append(parameterTypes[i].getSimpleName() + " " + paramNames.get(i));
-                }else {
-                    sb.append(parameterTypes[i].getSimpleName() + " " + paramNames.get(i) + ",");
+                logger.info("\n     ------> 请求接口：" + pjp.getTarget().getClass().getName() + "." + methodName + "()" +
+                        "\n     ------> 参数为：接口无参数");
+            } else {
+                List<String> paramNames =getParamNames(method);//参数名称
+                StringBuilder sb = new StringBuilder();
+                Class<?>[] parameterTypes = method.getParameterTypes();
+                sb.append("(");
+                for (int i = 0; i < parameterTypes.length;i++) {
+                    if ((i+1) == parameterTypes.length) {
+                        sb.append(parameterTypes[i].getSimpleName() + " " + paramNames.get(i));
+                    }else {
+                        sb.append(parameterTypes[i].getSimpleName() + " " + paramNames.get(i) + ",");
+                    }
                 }
-            }
-            sb.append(")");
-            String methodNameSb = MessageFormat.format(name, sb.toString());
-            JSONObject jsonObject = new JSONObject();
-            for (int i = 0; i < args.length; i++) {
-                jsonObject.put(paramNames.get(i),args[i]);
-            }
-            logger.info("\n     ------> 请求接口：" + methodNameSb +
+                sb.append(")");
+                String name = pjp.getTarget().getClass().getSimpleName() + "." + methodName + "{0}";
+                String methodNameSb = MessageFormat.format(name, sb.toString());
+                Object[] args = pjp.getArgs();
+                logger.info("\n     ------> 请求接口：" + methodNameSb +
+                            "\n     ------> 参数为：" + JSON.toJSONString(args)
+                );
+                JSONObject jsonObject = new JSONObject();
+                for (int i = 0; i < args.length; i++) {
+                    jsonObject.put(paramNames.get(i),args[i]);
+                }
+                Object result = pjp.proceed(args);
+                logger.info("\n     ------> 请求接口：" + methodNameSb +
                         "\n     ------> 参数为：" + jsonObject.toJSONString()
-                            + (result != null ? "\n     ------> 返回值为：" + JSON.toJSONString(result) : "")
-                    );
-        } catch (Exception e) {
-            logger.info("出现异常 = {}",e);
+                        + (result != null ? "\n     ------> 返回值为：" + JSON.toJSONString(result) : "接口无返回值")
+                );
+                return result;
+            }
+        } catch (Throwable throwable) {
+            logger.error("出现异常 = {}",throwable);
+            throw new RuntimeException(ResultBean.getFailResultString(500,throwable.getMessage()));
         }
+        return null;
     }
+
     //获取参数名称
     private List<String> getParamNames(Method method){
         List<String> paramNames = new ArrayList<>();
