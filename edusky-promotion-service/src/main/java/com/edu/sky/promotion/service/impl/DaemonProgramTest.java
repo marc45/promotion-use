ef6c6961cfa8029cae3e58bce354ca529ba30450
songwei <songwei@e-eduspace.com>
2017-11-13 13:08:55 +0800

    添加定时删除过期的优惠券：有过期时间的

--- edusky-promotion-service/src/main/java/com/edu/sky/promotion/service/impl/DaemonProgramTest.java
+++ edusky-promotion-service/src/main/java/com/edu/sky/promotion/service/impl/DaemonProgramTest.java
@@ -9,12 +9,13 @@ public class DaemonProgramTest {
     private static BlockingQueue<String> logQueue = new ArrayBlockingQueue<String>(queueCapacity);
 
     public static void main(String[] args) {
-        Logwriter logwriter = new Logwriter();
-        LogCleaner logCleaner = new LogCleaner();
-        logCleaner.setDaemon(true);
-
-        logwriter.start();
-        logCleaner.start();
+        for (int i = 0; i < 2; i++) {
+            Logwriter logwriter = new Logwriter();
+            logwriter.start();
+            LogCleaner logCleaner = new LogCleaner();
+            logCleaner.setDaemon(true);
+            logCleaner.start();
+        }
     }
 
     private static class Logwriter extends Thread{
@@ -23,7 +24,7 @@ public class DaemonProgramTest {
             for (int i = 0; i < queueCapacity; i++) {
                 try {
                     logQueue.put("" + i);
-                    System.out.println("日志已写入：内容为：" + logQueue);
+                    System.out.println("日志已写入：内容为：" +this.getName()+ logQueue);
                     Thread.sleep(500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
@@ -37,17 +38,11 @@ public class DaemonProgramTest {
         @Override
         public void run() {
             while (true) {
-                System.out.println("检查检查" + (++i));
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
                 if (logQueue.size() > 5) {
                     try {
                         logQueue.take();
-                        System.out.println("多余日志清除：当前内容为：" + logQueue);
-//                        Thread.sleep(500);
+                        System.out.println("多余日志清除：当前内容为：" +this.getName()+ logQueue);
+                        Thread.sleep(500);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
